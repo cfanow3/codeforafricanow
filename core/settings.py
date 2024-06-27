fa19673d31d7ddb6753752121f7b1cf0e9a77155
georgeokello <georgeokello335@gmail.com>
2024-06-27 15:05:18 +0300

    corrected basedir

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,11 @@
 from pathlib import Path
 import os
 import json
+from google.oauth2 import service_account
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
