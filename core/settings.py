7f7a181171058c59621afd01c10b4b72aa6c5b08
georgeokello <georgeokello335@gmail.com>
2024-06-29 08:52:08 +0300

    media root

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_ROOT = os.path.join('/codeforafrica/media', 'media') 
+MEDIA_ROOT = os.path.join('/codeforafrica', 'media') 
 MEDIA_URL ='/media/'
 
 STATIC_URL = 'static/'
